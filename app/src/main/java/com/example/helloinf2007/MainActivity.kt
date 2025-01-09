792a4214a5307cc7a8d4df924b282674bb69789f
WongQingLiang <qingliangwong89@gmail.com>
2025-01-09 14:38:50 +0800

    Changed name from Michael to Elon Musk

--- app/src/main/java/com/example/helloinf2007/MainActivity.kt
+++ app/src/main/java/com/example/helloinf2007/MainActivity.kt
@@ -4,19 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.helloinf2007.ui.theme.HelloINF2007Theme
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
